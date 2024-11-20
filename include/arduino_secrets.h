f32ea0acbedac5c0e2060fd232e5a0870c326e97
EtienneLeMagnifique <etiennevibert@gmail.com>
2024-11-20 12:35:15 -0500

    Secrets_Mise_à_jour

--- include/arduino_secrets.h
+++ include/arduino_secrets.h
@@ -1,7 +1,14 @@
-#define EAP_IDENTITY "abc@cmaisonneuve.qc.ca"
-  #define EAP_USERNAME "abc@cmaisonneuve.qc.ca"
-  #define EAP_PASSWORD "123456"
-  #define SECRET_SSID "eduroam"
+//  #define EAP_IDENTITY "abc@cmaisonneuve.qc.ca"
+//  #define EAP_USERNAME "abc@cmaisonneuve.qc.ca"
+//  #define EAP_PASSWORD "123456"
+//  #define SECRET_SSID "eduroam"
 
-  #define API_KEY "123456"
-  #define API_HOST "api.simplyprint.io/123456/endpoint/"
+#define WIFI_ENTERPRISE
+#define EAP_IDENTITY "cvwo@eva.eduroam.ca"
+#define EAP_USERNAME "cvwo@eva.eduroam.ca"
+#define EAP_PASSWORD "pyknm"
+#define SECRET_SSID "eduroam"
+
+  #define API_KEY "a833f716-fc14-4245-912e-e8fc07cb9684"
+ // #define API_HOST "api.simplyprint.io/12305/endpoint/"
+ #define API_HOST "api.simplyprint.io/12305/endpoint/"
